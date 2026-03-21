f3f3db7fdd57e7710139ba468a99929e83b01b97
CarolineT9 <caroltvrs@gmail.com>
2026-03-21 11:28:46 -0300

    fix: retira infra

--- infra/infra.tf
+++ infra/infra.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "seu-nome-terraform-state"
-    key            = "calculator/terraform.tfstate"
-    region         = "sa-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks"
-  }
-}
+# Backend configurado - deixar vazio para usar state local com -refresh=true
